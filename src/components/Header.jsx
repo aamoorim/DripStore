83ea0b1fbfed8752a08909ff82c43ede2f1b1ee4
amora <amorimmariana007@gmail.com>
2026-01-04 13:58:27 -0300

    Add colors variables and adjust it through the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
   return (
     <header>
-      <nav className="bg-[#FFFFFF] pl-26 pr-25 w-100% pt-10.5 pb-7.5">
+      <nav className="bg-white pl-26 pr-25 w-100% pt-10.5 pb-7.5">
         <div>
           {/* Container Superior */}
-          <div className='flex gap-15 items-center'>
-            <img src="../assets/logo-header.svg" alt="Logo da Digital College" />
+          <div className="flex gap-15 items-center">
+            <img
+              src="../assets/logo-header.svg"
+              alt="Logo da Digital College"
+            />
 
             {/* Input de Busca */}
             <form action="">
@@ -17,32 +19,41 @@ const Header = () => {
                 <input
                   type="text"
                   placeholder="Pesquisar produto..."
-                  className="w-140 bg-[#F5F5F5] pt-4 pb-4 pl-6 rounded-lg font-normal text-[#666666] focus:outline-3 focus:outline-[#C92071] tracking-wider"
+                  className="w-140 bg-light-gray-3 pt-4 pb-4 pl-6 rounded-lg font-normal text-dark-gray-3 focus:outline-nonetracking-wider"
                 />
-                <CiSearch className="absolute right-3 top-4.5 w-6 h-6 text-[#8F8F8F]" />
+                <CiSearch className="absolute right-3 top-4.5 w-6 h-6 text-light-gray" />
               </div>
             </form>
 
             {/* Links */}
-            <a href="*" className="underline underline-offset-2 text-[#474747] tracking-wider hover:text-[#C92071] transition-all duration-150">Cadastre-se</a>
+            <a
+              href="*"
+              className="underline underline-offset-2 text-dark-gray-2 tracking-wider hover:text-primary transition-all duration-150"
+            >
+              Cadastre-se
+            </a>
             <input
               type="button"
               value="Entrar"
-              className="bg-[#C92071] hover:bg-[#991956] transition-all duration-200 rounded-lg w-28.5 h-10 text-[#FFFFFF] cursor-pointer tracking-wider"
+              className="bg-primary hover:bg-tertiary transition-all duration-200 rounded-lg w-28.5 h-10 text-white cursor-pointer tracking-wider"
             />
-            <img src="../assets/mini-cart.svg" alt="Imagem de um carrinho de compras" className="cursor-pointer" />
+            <img
+              src="../assets/mini-cart.svg"
+              alt="Imagem de um carrinho de compras"
+              className="cursor-pointer"
+            />
           </div>
 
           {/* Container Inferior de Navegação */}
           <div className="flex mt-12">
-            <ul className="flex gap-8 text-[#474747] tracking-wider">
+            <ul className="flex gap-8 text-dark-gray-2 tracking-wider">
               <li>
                 <NavLink
                   to="/"
                   className={({ isActive }) =>
                     isActive
-                      ? "font-bold text-[#C92071] underline underline-offset-4 decoration-2" // Estilo ativo (quando a página Home está ativa)
-                      : "hover:font-bold hover:text-[#C92071] hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
+                      ? "font-bold text-primary underline underline-offset-4 decoration-2" // Estilo ativo (quando a página Home está ativa)
+                      : "hover:font-bold hover:text-primary hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
                   }
                 >
                   Home
@@ -53,8 +64,8 @@ const Header = () => {
                   to="/produtos"
                   className={({ isActive }) =>
                     isActive
-                      ? "font-bold text-[#C92071] underline underline-offset-4 decoration-2 " // Estilo ativo (quando a página Produtos está ativa)
-                      : "hover:font-bold hover:text-[#C92071] hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
+                      ? "font-bold text-primary underline underline-offset-4 decoration-2 " // Estilo ativo (quando a página Produtos está ativa)
+                      : "hover:font-bold hover:text-primary hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
                   }
                 >
                   Produtos
@@ -65,8 +76,8 @@ const Header = () => {
                   to="/categorias"
                   className={({ isActive }) =>
                     isActive
-                      ? "font-bold text-[#C92071] underline decoration-2 underline-offset-4" // Estilo ativo (quando a página Categorias está ativa)
-                      : "hover:font-bold hover:text-[#C92071] hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
+                      ? "font-bold text-primary underline decoration-2 underline-offset-4" // Estilo ativo (quando a página Categorias está ativa)
+                      : "hover:font-bold hover:text-primary hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
                   }
                 >
                   Categorias
@@ -77,8 +88,8 @@ const Header = () => {
                   to="/meus-pedidos"
                   className={({ isActive }) =>
                     isActive
-                      ? "font-bold text-[#C92071] underline decoration-2 underline-offset-4" // Estilo ativo (quando a página Meus Pedidos está ativa)
-                      : "hover:font-bold hover:text-[#C92071] hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
+                      ? "font-bold text-primary underline decoration-2 underline-offset-4" // Estilo ativo (quando a página Meus Pedidos está ativa)
+                      : "hover:font-bold hover:text-primary hover:underline underline-offset-4 cursor-pointer transition-all duration-200 decoration-2"
                   }
                 >
                   Meus Pedidos
