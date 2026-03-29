da628bfc24fe8b7b94d600c9d117e4f3171ecee4
amora <amorimmariana007@gmail.com>
2026-03-29 20:28:26 -0300

    Corrige caminho imagem login e registro

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -75,7 +75,7 @@ const Register = () => {
 
         <div className="hidden md:flex justify-center items-center">
           <img
-            src="../public/tenis-login.png"
+            src="../public/tenis-login-2.png"
             alt="Tênis"
             className="max-w-lg ml-10 w-full rotate-y-360 rotate-40"
           />
